16ebf8db42f2ff35a1ee87dc2f4fbdac03546a60
Alexandre Aubin <alex.aubin@mailoo.org>
2017-07-03 23:33:27 +0200

    Adding roadmap for 2.7

--- roadmap/fetch.py
+++ roadmap/fetch.py
@@ -3,12 +3,15 @@
 import requests
 import json
 
+roadmaps = { "2.6.x": 11, "2.7.x": 13 }
+
 def get_roadmapissues():
 
-    roadmapissues = requests.get("https://dev.yunohost.org/issues.xml?fixed_version_id=11&status_id=*&limit=100", verify=False)
+    for roadmap, id in roadmaps.items():
 
+        issues = requests.get("https://dev.yunohost.org/issues.xml?fixed_version_id="+str(id)+"&status_id=*&limit=100", verify=False)
 
-    with open("data/raw_roadmapissues.xml", "w") as f:
-        f.write(roadmapissues.text)
+        with open("data/raw_"+roadmap+"_issues.xml", "w") as f:
+            f.write(issues.text)
 
 get_roadmapissues()
