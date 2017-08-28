ae05f42bb726d0ca49b132ecfe8561cf19a0d657
Alexandre Aubin <alex.aubin@mailoo.org>
2017-08-29 00:24:31 +0200

    Improve sorting of appci

--- appci/analyze.py
+++ appci/analyze.py
@@ -20,21 +20,26 @@ def main():
 
         applevel = appdata.split("\n")[1][0]
         appdata = appdata.split("\n")[0]
-        data["level"] = applevel
+        data["level"] = int(applevel)
 
+        statusescore = 0
         for c in appdata:
             if c == "0":
                 s = "danger"
+                statusescore -= 1
             elif c == "1":
                 s = "success"
+                statusescore += 1
             else:
                 s = "unknown"
 
             data["statuses"].append(s)
+        
+        data["statusescore"] = statusescore
 
         apps.append(data)
 
-    apps.sort(key=lambda a: a["level"], reverse=True)
+    apps.sort(key=lambda a: (a["level"], a["statusescore"], a["name"])), reverse=True)
 
     with open("apps.json", "w") as f: 
         json.dump(apps, f)
