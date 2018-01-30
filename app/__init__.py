b55ffb6f313c0635b23eddcc6b4ab0a369c9d759
Alexandre Aubin <alex.aubin@mailoo.org>
2018-01-30 04:30:52 +0100

    Don't autoflush/autocommit ?

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+db = SQLAlchemy(session_options={"autoflush": False, "autocommit": False, "expire_on_commit": False})
 
 def create_app():
 
